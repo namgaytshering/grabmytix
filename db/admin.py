1dd855456e29f6c838328ec23345386d59d387fb
Namgay Tshering <namgay2340@gmail.com>
2025-11-21 10:40:05 +1100

    update

--- db/admin.py
+++ db/admin.py
@@ -9,6 +9,14 @@ admin.site.register(Country)
 admin.site.register(State)
 admin.site.register(EventType)
 admin.site.register(Event)
-admin.site.register(Booking)
+
 admin.site.register(EventAccess)
 admin.site.register(FilmShowAccess)
+
+# Custom admin for Booking with filter by event
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('full_name','title','attended', 'type','theater_name','state__name')  # adjust fields as needed
+    list_filter = ('event',)  # adds sidebar filter by event
+
+# Register Booking with custom admin
+admin.site.register(Booking, BookingAdmin)
